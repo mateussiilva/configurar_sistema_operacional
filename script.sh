ec9ba76b2a7e85866a87681ccab5eb1b5ad3ded3
mateussiilva <mateuspython3010>
2023-11-15 22:01:00 -0300

    Subindo shellscript

---
+++ script.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+
+
+
+
+
+
+NOME_USUARIO="" #Nome de usurio 
+EMAIL_USUARIO="" #Email de usurio 
+
+
+configurar_git() {
+    echo "Vamos Configurar o GIT"
+}
+
+
+
+
+# BLOCO PRINCIPAL
+configurar_git
